0aa033a8fd207f7c55efbfbd6be5a6a4ef36e1ba
temilaj <temi@temilajumoke.com>
2024-08-15 18:19:51 -0700

    move input embedding layer to separate class

---
+++ layers/input_embedding.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+import math
+
+class InputEmbedding(nn.Module):
+
+    
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        """
+            d_model: dimension of the vector in the model
+            vocab_size: number of words in the vocabulary
+        """
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        # (batch, seq_len) --> (batch, seq_len, d_model)
+        # Multiply weights in embedding layer by sqrt(d_model) to scale the embeddings according to the paper
+        return self.embedding(x) * math.sqrt(self.d_model)
