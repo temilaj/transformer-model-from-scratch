2d5c44f45696a362f9ca5710777da1f84b72fd35
temilaj <temi@temilajumoke.com>
2024-08-15 18:15:36 -0700

    add  feed forward layer

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+import math
 
 class InputEmbeddings(nn.Module):
 
@@ -12,3 +14,8 @@ class InputEmbeddings(nn.Module):
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        # (batch, seq_len) --> (batch, seq_len, d_model)
+        # Multiply weights in embedding layer by sqrt(d_model) to scale the embeddings according to the paper
+        return self.embedding(x) * math.sqrt(self.d_model)
