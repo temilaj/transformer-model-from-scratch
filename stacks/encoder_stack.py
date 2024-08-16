376c883b8b023ca3740d5e0d43105f02a65f6dbe
temilaj <temi@temilajumoke.com>
2024-08-15 19:23:12 -0700

    add encoder stack

---
+++ stacks/encoder_stack.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from layers.normalization import LayerNormalization
+
+class Encoder(nn.Module):
+
+    def __init__(self, features: int, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
